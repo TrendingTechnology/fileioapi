84698ba013c99cc0303cb2319a3c0c72a4cda6f2
nkot56297 <95204402+nkot56297@users.noreply.github.com>
2021-12-18 03:24:50 -0300

    Update main.py

--- cli/main.py
+++ cli/main.py
@@ -1,25 +1,20 @@
 import getopt, sys
 import fileioapi
+
 argumentList = sys.argv[1:]
-
 options = "::: "
-
 long_options = ["Help", "upload", "download"]
 
 try:
 	arguments, values = getopt.getopt(argumentList, options, long_options)
-	
 	for currentArgument, currentValue in arguments:
 
 		if currentArgument in ("-h", "--Help"):
 			print ("Displaying Help")
-			
 		elif currentArgument in ("-upload", "--upload"):
-      fileioapi.upload("image.png", expiry="12w")
-			
+			fileioapi.upload("image.png", expiry="12w")
 		elif currentArgument in ("-download", "--download"):
-      fileioapi.download(resp['link'], filename='downloaded.png')
-      
+			fileioapi.download(resp['link'], filename='downloaded.png')
 		elif currentArgument in ("-o", "--Output"):
 			print (("Enabling special output mode (% s)") % (currentValue))
 			
